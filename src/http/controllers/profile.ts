d3e7a201d285fd669cbe863daf752e4c49ed6951
killer-cf <costa.kilder@live.com>
2023-06-07 10:13:50 -0300

    perfil controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,15 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(req: FastifyRequest, res: FastifyReply) {
-  await req.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  console.log(req.user.sub)
+  const { user } = await getUserProfile.execute({ user_id: req.user.sub })
 
-  return res.status(200).send()
+  return res.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
